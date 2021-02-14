30f9d6ebdd8a461bda13b10519989e1312edf40a
Ahmed <>
2021-02-14 13:22:41 +1100

    initial commit

---
+++ app/src/main/java/com/emazdoor/zeller/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.emazdoor.zeller.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.emazdoor.zeller.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
